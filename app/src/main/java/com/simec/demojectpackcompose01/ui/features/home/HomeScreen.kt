55317d592c69473705d361b0fa34ff944b523c58
Emdadul Haque Siam <ehsiam75@gmail.com>
2025-10-16 17:09:38 +0600

    Color change

--- app/src/main/java/com/simec/demojectpackcompose01/ui/features/home/HomeScreen.kt
+++ app/src/main/java/com/simec/demojectpackcompose01/ui/features/home/HomeScreen.kt
@@ -24,14 +24,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
