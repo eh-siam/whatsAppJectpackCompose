b905d998a146ce056b787c41f39a3e56f12027eb
Emdadul Haque Siam <ehsiam75@gmail.com>
2025-10-26 19:20:48 +0600

    Settings -> Account Action Screen

--- app/src/main/java/com/simec/e_planner/ui/features/settings/account_actions/AccountActionScreen.kt
+++ app/src/main/java/com/simec/e_planner/ui/features/settings/account_actions/AccountActionScreen.kt
@@ -4,10 +4,12 @@ import com.simec.e_planner.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,43 +17,60 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.simec.e_planner.ui.components.CustomAppBarWithCard
-
-/**
- * Created by Emdadul Haque Siam on 09,October,2025
- * Copyright (c): SIMEC System Ltd.
- */
+import com.simec.e_planner.ui.theme.NunitoSemiBold
 
 @Composable
-fun AccountActionScreen(navController: NavHostController){
+fun AccountActionScreen(navController: NavHostController) {
     Scaffold(
-        topBar = { CustomAppBarWithCard(
-            title = "Account Action",
-            onBackClick = { navController.popBackStack() }
-        )}
-    ){ innerPadding ->
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding)
-            .background(Color.White)){
+        topBar = {
+            CustomAppBarWithCard(
+                title = "Account Action",
+                onBackClick = { navController.popBackStack() }
+            )
         }
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(top = 50.dp)
+                .padding(innerPadding)
+                .background(Color.White)
+                .padding(top = 20.dp)
         ) {
-            Card (
+            Card(
                 modifier = Modifier
-                    .size(height = 60.dp, width = 400.dp)
-                    .padding(horizontal = 16.dp)
-            ){
+                    .fillMaxWidth()
+                    .height(160.dp)
+                    .padding(horizontal = 16.dp, vertical = 10.dp)
+            ) {
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(color = colorResource(id = R.color.MainCardColor))
+                        .background(colorResource(id = R.color.MainCardColor))
+                        .padding(16.dp)
                 ) {
-
+                    Text(
+                        text = "Change Password",
+                        color = colorResource(id = R.color.white),
+                        fontFamily = NunitoSemiBold,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = "Delete / Deactivate",
+                        color = colorResource(id = R.color.white),
+                        fontFamily = NunitoSemiBold,
+                        fontSize = 16.sp,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                    Text(
+                        text = "Logout",
+                        color = colorResource(id = R.color.white),
+                        fontFamily = NunitoSemiBold,
+                        fontSize = 16.sp,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
                 }
             }
         }
@@ -60,6 +79,6 @@ fun AccountActionScreen(navController: NavHostController){
 
 @Preview(showBackground = true)
 @Composable
-fun AccountActionPreview(){
+fun AccountActionPreview() {
     AccountActionScreen(navController = NavHostController(LocalContext.current))
 }
